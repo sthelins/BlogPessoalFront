d12b1b36a48270ba81f45bcdc069c2a4eb9b582d
Sthefany <sthefanylins_albuquerque@hotmail.com>
2022-08-28 21:43:46 -0300

    iniciando o nav, home e footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import Navbar from './componentes/estaticos/navbar/Navbar'
+import Footer from './componentes/estaticos/footer/Footer'
+import {Grid} from '@material-ui/core';
 import logo from './logo.svg';
 import Home from './paginas/home/Home'
 import './App.css';
 
 function App() {
   return (
-      <Home />
+      <>
+          <Navbar />
+          <Home />
+          <Footer />
+      </>
     );
 }
 
